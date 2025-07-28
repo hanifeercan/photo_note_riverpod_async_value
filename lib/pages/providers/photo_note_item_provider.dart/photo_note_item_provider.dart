371a11d506634efff8dc6ed404eca7f07471cbb5
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2025-07-28 16:44:50 +0300

    Create PhotoNoteItemProvider

---
+++ lib/pages/providers/photo_note_item_provider.dart/photo_note_item_provider.dart
@@ -0,0 +1,9 @@
+import 'package:photo_note_riverpod_async_value/models/photo_note.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'photo_note_item_provider.g.dart';
+
+@Riverpod(dependencies: [])
+PhotoNote photoNoteItem(PhotoNoteItemRef ref) {
+  throw UnimplementedError();
+}
